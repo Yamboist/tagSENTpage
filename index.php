f66e0d2423d83fbcbdd2b12e57cf65cb393f73c7
yamplusplus@gmail.com <yamplusplus@gmail.com>
2014-09-30 01:10:19 +0800

    new commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+	</head>
+
+	<body>
+		<div class="center">
+			<div id="header">
+				<h1>tagSENT &middot; tagalog sentiment analyzer</h1>
+			</div>
+			<div id="prediction-forms">
+				<h5>Enter the text you wanted to be analyzed: </h5>
+				<small>*Just a note: please add spaces besides every punctuations ...</small>
+				<br>
+				<textarea></textarea>
+				<button id="analyze">Analyze</button>
+			</div>
+		
+			<div id="results">
+				<p>Results:</p>
+				
+				<p><small>General Sentiment: POSITIVE </small> | <small>chart stuff: 55% positive , 45% negative</small></p>
+
+				<p><small>Positives: </small></p>
+				<p><small>Negatives: </small></p>
+			</div>
+		</div>
+	</body>
+
+
+</html>
+
+
+<script>
+
+</script>
